208748821973c9f175dd9269e918c7fa90b3d036
Tanay Kapoor <itstkaay@gmail.com>
2021-05-20 18:40:43 +0530

    Updates

---
+++ precache-manifest.03499ad3b37309e8ad8a7c9529f40a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fef983603e8265f159d69655f168cf",
+    "url": "/React-Covid-Tracker.git/index.html"
+  },
+  {
+    "revision": "23a98c43f26f15bfae9e",
+    "url": "/React-Covid-Tracker.git/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "419e45416d478b073ab6",
+    "url": "/React-Covid-Tracker.git/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "23a98c43f26f15bfae9e",
+    "url": "/React-Covid-Tracker.git/static/js/2.4521feac.chunk.js"
+  },
+  {
+    "revision": "88ab8e9bfa1a02a6d25f3aa28a50aeac",
+    "url": "/React-Covid-Tracker.git/static/js/2.4521feac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419e45416d478b073ab6",
+    "url": "/React-Covid-Tracker.git/static/js/main.b18d5a8c.chunk.js"
+  },
+  {
+    "revision": "9af4b477cd1eee34a05e",
+    "url": "/React-Covid-Tracker.git/static/js/runtime-main.259f67b1.js"
+  }
+]);
